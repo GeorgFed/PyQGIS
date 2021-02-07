055d85c13486dbd8bf062919300ec11fb518a9de
Егор Федяев <azurit78@gmail.com>
2021-02-07 19:23:37 +0300

    Update see_attributes.py

--- Vector/scripts/see_attributes.py
+++ Vector/scripts/see_attributes.py
@@ -1,4 +1,6 @@
 layer = QgsVectorLayer("D:/hse/GIS/qgis_vector/nyc/NYC_MUSEUMS_GEO.shp", "New York City Museums", "ogr")
+# Получаем итератор по свойствам слоя
 features = layer.getFeatures()
+# Проходим по итератору и отображаем свойства
 f = feature.__next__()
 f.attributes()
