185c890ea16d1c47433a43214b6ca14127c6a7b1
Егор Федяев <azurit78@gmail.com>
2021-02-07 19:43:45 +0300

    Update filter_attributes.py

--- Vector/scripts/filter_attributes.py
+++ Vector/scripts/filter_attributes.py
@@ -1,5 +1,8 @@
 lyrPts = QgsVectorLayer("D:/hse/GIS/project_qgis_repository/PyQGIS/Vector/scripts/nyc/NYC_MUSEUMS_GEO.shp", "Museums", "ogr")
 QgsProject.instance().addMapLayers([lyrPts])
+# Фильтруем объекты по ZIP-коду 
 selection = lyrPts.getFeatures(QgsFeatureRequest().setFilterExpression(u'"ZIP" = 10002'))
+# Выделяем отфильторванные точки
 lyrPts.selectByIds([s.id() for s in selection])
+# Приближаем выделенные точки
 iface.mapCanvas().zoomToSelected()
