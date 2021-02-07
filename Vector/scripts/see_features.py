0d1181da4f8f710781515a3179abe40d6baf53a5
Егор Федяев <azurit78@gmail.com>
2021-02-07 19:14:16 +0300

    Update see_features.py

--- Vector/scripts/see_features.py
+++ Vector/scripts/see_features.py
@@ -1,5 +1,9 @@
+# Создаем слой
 layer = QgsVectorLayer("D:/hse/GIS/qgis_vector/nyc/NYC_MUSEUMS_GEO.shp", "New York City Museums", "ogr")
+# Получаем итератор по характеристикам слоя
 features = layer.getFeatures()
+# Проходим по итератору
 f = features.__next__()
 g = f.geometry()
+# Отображаем геометрию характеристики как точку
 print(g.asPoint())
