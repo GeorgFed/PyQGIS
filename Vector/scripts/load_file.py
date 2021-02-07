d5bffd5dac6e92a9f866e9b92c84704526bcb43e
Егор Федяев <azurit78@gmail.com>
2021-02-07 19:04:21 +0300

    Update load_file.py

--- Vector/scripts/load_file.py
+++ Vector/scripts/load_file.py
@@ -1,4 +1,8 @@
+# Используя путь к векторном файлу NYC_MUSEUMS_GEO.shp создаем слой
 layer = QgsVectorLayer("<NYC_MUSEUMS_GEO.shp FULL PATH>", "New York City Museums", "ogr")
+# Проверяем корректность создания слоя
 if not layer.isValid():
+  # Выводим ошибку создания слоя
   print("Layer {} did not load".format(layer.name()))
+# Добавляем векторный слой на карту
 QgsProject.instance().addMapLayers([layer])
